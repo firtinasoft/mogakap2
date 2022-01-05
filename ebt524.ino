ae5efa5a12dd2d0ebbeea0cad4b83fd7f115d277
= <=>
2022-01-06 00:34:50 +0300

    ilk dosyalar

---
+++ ebt524.ino
@@ -0,0 +1,28 @@
+//EBT524 - PROJE
+#include "FirebaseESP8266.h"
+#include <ESP8266WiFi.h>
+#define FIREBASE_HOST "https://mogakap-aa4ee-default-rtdb.firebaseio.com"
+#define FIREBASE_AUTH "AIzaSyAeEZ06WyjyhExpKnv--FPCJ6t-c3VTPX0"
+#define WIFI_SSID "SATSOKAPI"
+#define WIFI_PASSWORD "123456789"
+FirebaseData mogakap;
+void setup()
+{
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+ pinMode(D1,OUTPUT);
+ digitalWrite(D1,LOW);
+}
+void loop()
+{
+  if(Firebase.getString(mogakap, "/ac"))
+  {
+    if (mogakap.stringData()=="1")
+    {
+      digitalWrite(D1,HIGH);
+      delay (500);
+      digitalWrite(D1,LOW);
+    }
+    else {  digitalWrite(D1,LOW); }
+  }
